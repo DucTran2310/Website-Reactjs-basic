01ca264a753a5cb0f1915ba82e3ae0a0e0546e4e
DucTran2310 <trananhduc23102000@gmail.com>
2021-09-05 22:51:04 +0700

    Update project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import 'App.css';
 
 function App() {
     return (
-        <div className="App">
-            <Navbar />
-        </div>
+        <>
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact />
+                </Switch>
+            </Router>
+        </>
     );
 }
 
