9c0728ff16a08c636fd9aea6c0bec52c49f88214
DucTran2310 <trananhduc23102000@gmail.com>
2021-09-07 12:37:35 +0700

    Update App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import Home from './features/Home';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import 'App.css';
+import Services from 'features/Service/Service';
+import Products from 'features/Product/Product';
+import SignUp from 'features/Sign-Up/SignUp';
 
 function App() {
     return (
@@ -10,7 +14,10 @@ function App() {
             <Router>
                 <Navbar />
                 <Switch>
-                    <Route path="/" exact />
+                    <Route path="/" exact component={Home} />
+                    <Route path='/services' component={Services} />
+                    <Route path='/products' component={Products} />
+                    <Route path='/sign-up' component={SignUp} />
                 </Switch>
             </Router>
         </>
