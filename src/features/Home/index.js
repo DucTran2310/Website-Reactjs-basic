e7d40cf25cf294419830a3e96987d579197e802c
DucTran2310 <trananhduc23102000@gmail.com>
2021-09-07 12:37:00 +0700

    Add pages

---
+++ src/features/Home/index.js
@@ -0,0 +1,17 @@
+import Cards from 'components/Cards/Cards';
+import Footer from 'components/Footer/Footer';
+import Hero from 'components/HeroSection/Hero';
+import '../../App.css';
+
+function Home() {
+    return (
+        <>
+            
+            <Hero/>
+            <Cards />
+            <Footer />
+        </>
+    );
+}
+
+export default Home;
